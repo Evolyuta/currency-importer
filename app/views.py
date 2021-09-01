be1ce748704125197887c4ff8f4780e28d26a9f2
evolyuta <dl@nimax.ru>
2021-09-01 17:01:31 +0300

    add hardcoded page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def currency_importer_view(request, *args, **kwargs):
+    return render(request, 'currency-importer.html', {})
--- currencyimporter/urls.py
+++ currencyimporter/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from app.views import currency_importer_view
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('currency-importer/', currency_importer_view, name='currency-importer'),
     path('admin/', admin.site.urls),
 ]
