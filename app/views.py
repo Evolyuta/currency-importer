2dfa1f4c0d97e72bf2167aecb61c4522bf47c20d
evolyuta <dl@nimax.ru>
2021-09-02 19:11:06 +0300

    fix setting context in view function

--- app/views.py
+++ app/views.py
@@ -4,16 +4,14 @@ from services.currency_service import CurrencyService
 
 
 def currency_importer_view(request):
-    context = {}
     currency_service = CurrencyService()
 
     if request.method == 'POST':
-
         currency_service.import_currency_list()
         currency_service.import_currency_compare_list()
-    else:
-        context = {
-            "object_list": currency_service.get_data_for_compare_view()
-        }
+
+    context = {
+        "object_list": currency_service.get_data_for_compare_view()
+    }
 
     return render(request, 'currency-importer.html', context)
