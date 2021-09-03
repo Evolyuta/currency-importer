9482e6c5bf475519e5b61dade29599660b243303
evolyuta <dl@nimax.ru>
2021-09-03 13:11:57 +0300

    add displaying currencies names in comparison admin table

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from .models import Currency, CurrencyComparison
+from .models import CurrencyComparison
 
-admin.site.register(Currency)
-admin.site.register(CurrencyComparison)
+
+# Customizing CurrencyComparison model for admin panel
+class CurrencyComparisonAdmin(admin.ModelAdmin):
+    # Admin model table fields
+    list_display = [
+        'currency_title',
+        'compared_currency_title',
+        'ratio',
+    ]
+
+
+admin.site.register(CurrencyComparison, CurrencyComparisonAdmin)
--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class Currency(models.Model):
     title = models.CharField(max_length=120)
     code = models.CharField(max_length=120, null=True)
 
+    # Many-to-many relationship to itself
     compared_currencies = models.ManyToManyField(
         'self',
         through='CurrencyComparison',
@@ -17,3 +18,15 @@ class CurrencyComparison(models.Model):
     from_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='from_currency')
     to_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='to_currency')
     ratio = models.FloatField()
+
+    # Getting title of comparing currency
+    def currency_title(self):
+        return self.from_currency.title
+
+    # Getting title of compared currency
+    def compared_currency_title(self):
+        return self.to_currency.title
+
+    class Meta:
+        # Singular model label name
+        verbose_name = 'Currency comparison'
